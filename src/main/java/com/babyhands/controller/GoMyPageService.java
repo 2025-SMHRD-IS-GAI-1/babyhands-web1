ca973386105ef1d43d5206927cf4ba7990b60acb
pobiq <hoon2207@naver.com>
2025-11-03 15:59:05 +0900

    마이페이지 정보 불러오는 백엔드 작업 구현

---
+++ src/main/java/com/babyhands/controller/GoMyPageService.java
@@ -0,0 +1,38 @@
+package com.babyhands.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.babyhands.dao.MemberDAO;
+import com.babyhands.dao.SlTestDAO;
+import com.babyhands.dto.MemberScoreRank;
+import com.babyhands.frontController.Command;
+import com.babyhands.vo.MemberVO;
+
+public class GoMyPageService implements Command {
+
+	@Override
+	public String execute(HttpServletRequest request, HttpServletResponse response) {
+		
+		HttpSession session = request.getSession();
+		
+		if (session == null || session.getAttribute("loginVO") == null) {
+            return "redirect:/Gologin.do";
+        }
+		
+		MemberVO loginVO = (MemberVO) session.getAttribute("loginVO");
+		String memberId = loginVO.getMemberId();
+		
+		MemberDAO dao = new MemberDAO();
+		SlTestDAO slTestdao = new SlTestDAO();
+		MemberVO member = dao.getMemberInfo(memberId);
+		MemberScoreRank memberScoreRank = slTestdao.getScoreRank(memberId);
+		
+		request.setAttribute("member", member);
+		request.setAttribute("memberScoreRank", memberScoreRank);
+		
+		return "mypage.jsp";
+	}
+
+}
